c71df63f33d120467f3712afbf0e7d2b69bdcad2
Kazuhiro Sera <seratch@gmail.com>
2020-08-26 18:15:24 +0900

    Add process_before_response=True

--- samples/google_cloud_functions/main.py
+++ samples/google_cloud_functions/main.py
@@ -6,7 +6,8 @@ logging.basicConfig(level=logging.DEBUG)
 
 from slack_bolt import App
 
-app = App()
+# process_before_response must be True when running on FaaS
+app = App(process_before_response=True)
 
 
 @app.command("/hello-bolt-python-gcp")
