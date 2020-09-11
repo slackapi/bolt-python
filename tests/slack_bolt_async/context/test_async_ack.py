df62485549780659bec8c2d2f97ae1bc37494dcf
Kazuhiro Sera <seratch@gmail.com>
2020-09-11 14:42:25 +0900

    Add tests for ack etc

--- tests/slack_bolt_async/context/test_async_ack.py
+++ tests/slack_bolt_async/context/test_async_ack.py
@@ -22,6 +22,76 @@ class TestAsyncAsyncAck:
             '{"text": "foo", "blocks": [{"type": "divider"}]}',
         )
 
+    sample_attachments = [
+        {
+            "fallback": "Plain-text summary of the attachment.",
+            "color": "#2eb886",
+            "pretext": "Optional text that appears above the attachment block",
+            "author_name": "Bobby Tables",
+            "author_link": "http://flickr.com/bobby/",
+            "author_icon": "http://flickr.com/icons/bobby.jpg",
+            "title": "Slack API Documentation",
+            "title_link": "https://api.slack.com/",
+            "text": "Optional text that appears within the attachment",
+            "fields": [{"title": "Priority", "value": "High", "short": False}],
+            "image_url": "http://my-website.com/path/to/image.jpg",
+            "thumb_url": "http://example.com/path/to/thumb.png",
+            "footer": "Slack API",
+            "footer_icon": "https://platform.slack-edge.com/img/default_application_icon.png",
+            "ts": 123456789,
+        }
+    ]
+
+    @pytest.mark.asyncio
+    async def test_attachments(self):
+        ack = AsyncAck()
+        response: BoltResponse = await ack(
+            text="foo", attachments=self.sample_attachments
+        )
+        assert (response.status, response.body) == (
+            200,
+            '{"text": "foo", '
+            '"attachments": [{"fallback": "Plain-text summary of the attachment.", "color": "#2eb886", "pretext": "Optional text that appears above the attachment block", "author_name": "Bobby Tables", "author_link": "http://flickr.com/bobby/", "author_icon": "http://flickr.com/icons/bobby.jpg", "title": "Slack API Documentation", "title_link": "https://api.slack.com/", "text": "Optional text that appears within the attachment", "fields": [{"title": "Priority", "value": "High", "short": false}], "image_url": "http://my-website.com/path/to/image.jpg", "thumb_url": "http://example.com/path/to/thumb.png", "footer": "Slack API", "footer_icon": "https://platform.slack-edge.com/img/default_application_icon.png", "ts": 123456789}]'
+            "}",
+        )
+
+    sample_options = [{"text": {"type": "plain_text", "text": "Maru"}, "value": "maru"}]
+
+    @pytest.mark.asyncio
+    async def test_options(self):
+        ack = AsyncAck()
+        response: BoltResponse = await ack(text="foo", options=self.sample_options)
+        assert response.status == 200
+        assert (
+            response.body
+            == '{"options": [{"text": {"type": "plain_text", "text": "Maru"}, "value": "maru"}]}'
+        )
+
+    sample_option_groups = [
+        {
+            "label": {"type": "plain_text", "text": "Group 1"},
+            "options": [
+                {"text": {"type": "plain_text", "text": "Option 1"}, "value": "1-1"},
+                {"text": {"type": "plain_text", "text": "Option 2"}, "value": "1-2"},
+            ],
+        },
+        {
+            "label": {"type": "plain_text", "text": "Group 2"},
+            "options": [
+                {"text": {"type": "plain_text", "text": "Option 1"}, "value": "2-1"},
+            ],
+        },
+    ]
+
+    @pytest.mark.asyncio
+    async def test_option_groups(self):
+        ack = AsyncAck()
+        response: BoltResponse = await ack(
+            text="foo", option_groups=self.sample_option_groups
+        )
+        assert response.status == 200
+        assert response.body.startswith('{"option_groups":')
+
     @pytest.mark.asyncio
     async def test_response_type(self):
         ack = AsyncAck()
