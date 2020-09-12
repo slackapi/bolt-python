78f21341a793cd2912115f2e15ad7b7e32568a1a
Kazuhiro Sera <seratch@gmail.com>
2020-09-12 18:31:41 +0900

    Add tests for Tornado adapter

---
+++ tests/adapter_tests/test_tornado_oauth.py
@@ -0,0 +1,43 @@
+import pytest
+import tornado
+from tornado.httpclient import HTTPRequest, HTTPResponse
+from tornado.testing import AsyncHTTPTestCase
+from tornado.web import Application
+
+from slack_bolt.adapter.tornado import SlackOAuthHandler
+from slack_bolt.app import App
+from slack_bolt.oauth.oauth_settings import OAuthSettings
+from tests.utils import remove_os_env_temporarily, restore_os_env
+
+signing_secret = "secret"
+
+app = App(
+    signing_secret=signing_secret,
+    oauth_settings=OAuthSettings(
+        client_id="111.111", client_secret="xxx", scopes=["chat:write", "commands"],
+    ),
+)
+
+
+class TestTornado(AsyncHTTPTestCase):
+    def get_app(self):
+        return Application([("/slack/install", SlackOAuthHandler, dict(app=app))])
+
+    def setUp(self):
+        AsyncHTTPTestCase.setUp(self)
+        self.old_os_env = remove_os_env_temporarily()
+
+    def tearDown(self):
+        AsyncHTTPTestCase.tearDown(self)
+        restore_os_env(self.old_os_env)
+
+    @tornado.testing.gen_test
+    async def test_oauth(self):
+        request = HTTPRequest(
+            url=self.get_url("/slack/install"), method="GET", follow_redirects=False
+        )
+        try:
+            response: HTTPResponse = await self.http_client.fetch(request)
+            assert response.code == 302
+        except tornado.httpclient.HTTPClientError as e:
+            assert e.code == 302
