8e1a8819ea68a2ddce6233698293db595eeef517
Kazuhiro Sera <seratch@gmail.com>
2020-10-27 20:36:37 +0900

    Add Python 3.9 to the supported versions

--- setup.py
+++ setup.py
@@ -75,6 +75,7 @@ setuptools.setup(
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: Implementation :: CPython",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
