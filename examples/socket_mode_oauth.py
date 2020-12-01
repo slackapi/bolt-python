9658af4c5eca461b6582dc6da70d9b0b1e86c83c
Kazuhiro Sera <seratch@gmail.com>
2020-12-01 23:07:40 +0900

    version 1.2.0b1

--- examples/socket_mode_oauth.py
+++ examples/socket_mode_oauth.py
@@ -6,10 +6,13 @@ sys.path.insert(1, "..")
 # ------------------------------------------------
 
 import logging
+
+logging.basicConfig(level=logging.DEBUG)
+
 import os
 from slack_bolt.app import App
 from slack_bolt.oauth.oauth_settings import OAuthSettings
-from slack_bolt.adapter.socket_mode.websocket_client import SocketModeHandler
+from slack_bolt.adapter.socket_mode import SocketModeHandler
 
 app = App(
     signing_secret=os.environ["SLACK_SIGNING_SECRET"],
@@ -17,7 +20,7 @@ app = App(
         client_id=os.environ["SLACK_CLIENT_ID"],
         client_secret=os.environ["SLACK_CLIENT_SECRET"],
         scopes=os.environ["SLACK_SCOPES"].split(","),
-    )
+    ),
 )
 
 
@@ -32,13 +35,74 @@ def event_test(event, say):
     say(f"Hi there, <@{event['user']}>!")
 
 
-@app.shortcut("socket-mode")
-def global_shortcut(ack):
+def ack_shortcut(ack):
+    ack()
+
+
+def open_modal(body, client):
+    client.views_open(
+        trigger_id=body["trigger_id"],
+        view={
+            "type": "modal",
+            "callback_id": "socket_modal_submission",
+            "submit": {"type": "plain_text", "text": "Submit",},
+            "close": {"type": "plain_text", "text": "Cancel",},
+            "title": {"type": "plain_text", "text": "Socket Modal",},
+            "blocks": [
+                {
+                    "type": "input",
+                    "block_id": "q1",
+                    "label": {"type": "plain_text", "text": "Write anything here!",},
+                    "element": {"action_id": "feedback", "type": "plain_text_input",},
+                },
+                {
+                    "type": "input",
+                    "block_id": "q2",
+                    "label": {
+                        "type": "plain_text",
+                        "text": "Can you tell us your favorites?",
+                    },
+                    "element": {
+                        "type": "external_select",
+                        "action_id": "favorite-animal",
+                        "min_query_length": 0,
+                        "placeholder": {
+                            "type": "plain_text",
+                            "text": "Select your favorites",
+                        },
+                    },
+                },
+            ],
+        },
+    )
+
+
+app.shortcut("socket-mode")(ack=ack_shortcut, lazy=[open_modal])
+
+
+all_options = [
+    {"text": {"type": "plain_text", "text": ":cat: Cat"}, "value": "cat",},
+    {"text": {"type": "plain_text", "text": ":dog: Dog"}, "value": "dog",},
+    {"text": {"type": "plain_text", "text": ":bear: Bear"}, "value": "bear",},
+]
+
+
+@app.options("favorite-animal")
+def external_data_source_handler(ack, body):
+    keyword = body.get("value")
+    if keyword is not None and len(keyword) > 0:
+        options = [o for o in all_options if keyword in o["text"]["text"]]
+        ack(options=options)
+    else:
+        ack(options=all_options)
+
+
+@app.view("socket_modal_submission")
+def submission(ack):
     ack()
 
 
 if __name__ == "__main__":
-    logging.basicConfig(level=logging.DEBUG)
     SocketModeHandler(app, os.environ.get("SLACK_APP_TOKEN")).connect()
     app.start()
 
---
+++ slack_bolt/adapter/socket_mode/__init__.py
@@ -0,0 +1,2 @@
+# Don't add async module imports here
+from .builtin import SocketModeHandler  # noqa
