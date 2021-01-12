5cb3162b847eddb6cb7885b0b2bb600d581771ad
Kazuhiro Sera <seratch@gmail.com>
2021-01-12 22:34:34 +0900

    version 1.2.0rc2

--- slack_bolt/adapter/socket_mode/builtin/__init__.py
+++ slack_bolt/adapter/socket_mode/builtin/__init__.py
@@ -24,10 +24,14 @@ class SocketModeHandler(BaseSocketModeHandler):
         app_token: Optional[str] = None,
         logger: Optional[Logger] = None,
         web_client: Optional[WebClient] = None,
-        ping_interval: float = 10,
-        concurrency: int = 10,
         proxy: Optional[str] = None,
+        auto_reconnect_enabled: bool = True,
         trace_enabled: bool = False,
+        all_message_trace_enabled: bool = False,
+        ping_pong_trace_enabled: bool = False,
+        ping_interval: float = 10,
+        receive_buffer_size: int = 1024,
+        concurrency: int = 10,
     ):
         self.app = app
         self.app_token = app_token or os.environ["SLACK_APP_TOKEN"]
@@ -36,9 +40,13 @@ class SocketModeHandler(BaseSocketModeHandler):
             logger=logger if logger is not None else app.logger,
             web_client=web_client if web_client is not None else app.client,
             proxy=proxy if proxy is not None else app.client.proxy,
-            ping_interval=ping_interval,
-            concurrency=concurrency,
+            auto_reconnect_enabled=auto_reconnect_enabled,
             trace_enabled=trace_enabled,
+            all_message_trace_enabled=all_message_trace_enabled,
+            ping_pong_trace_enabled=ping_pong_trace_enabled,
+            ping_interval=ping_interval,
+            receive_buffer_size=receive_buffer_size,
+            concurrency=concurrency,
         )
         self.client.socket_mode_request_listeners.append(self.handle)
 
