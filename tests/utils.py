52e1380c7699b8a3a5f71946997bc72061149fca
Kazuhiro Sera <seratch@gmail.com>
2021-02-04 12:04:58 +0900

    Revert the defualt testing mdoe to threading mode for better macOS Big Sur compatibility

--- tests/utils.py
+++ tests/utils.py
@@ -21,6 +21,9 @@ def get_mock_server_mode() -> str:
     """
     mode = os.environ.get("BOLT_PYTHON_MOCK_SERVER_MODE")
     if mode is None:
-        return "multiprocessing"
+        # We used to use "multiprocessing"" for macOS until Big Sur 11.1
+        # Since 11.1, the "multiprocessing" mode started failing a lot...
+        # Therefore, we switched the default mode back to "threading".
+        return "threading"
     else:
         return mode
