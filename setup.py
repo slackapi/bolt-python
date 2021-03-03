fa0fedc87c9115f7d806f8d93d446bd511a548dd
Kazuhiro Sera <seratch@gmail.com>
2021-03-03 11:24:04 +0900

    version 1.4.2

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ setuptools.setup(
         exclude=["examples", "integration_tests", "tests", "tests.*",]
     ),
     include_package_data=True,  # MANIFEST.in
-    install_requires=["slack_sdk>=3.4,<4",],
+    install_requires=["slack_sdk>=3.4.1,<4",],
     setup_requires=["pytest-runner==5.2"],
     tests_require=test_dependencies,
     test_suite="tests",
