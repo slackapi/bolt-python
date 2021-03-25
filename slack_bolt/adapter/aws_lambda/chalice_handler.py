19d814e2ad08c95c404dec9560a0bbe38c1adcdd
Jeremy Lujan <jeremy.lujan@invitae.com>
2021-03-25 14:44:37 -0500

    Fix types in LocalLambdaClient

--- slack_bolt/adapter/aws_lambda/chalice_handler.py
+++ slack_bolt/adapter/aws_lambda/chalice_handler.py
@@ -4,9 +4,7 @@ from os import getenv
 
 from chalice.app import Request, Response, Chalice
 from chalice.config import Config
-from chalice.test import (
-    BaseClient, LambdaContext, InvokeResponse
-)
+from chalice.test import BaseClient, LambdaContext, InvokeResponse
 
 from slack_bolt.adapter.aws_lambda.chalice_lazy_listener_runner import (
     ChaliceLazyListenerRunner,
@@ -21,18 +19,23 @@ from slack_bolt.response import BoltResponse
 
 class LocalLambdaClient(BaseClient):
     """Lambda client implementing `invoke` for use when running with Chalice CLI"""
-    def __init__(self, app, config):
-        # type: (Chalice, Config) -> None
+
+    def __init__(self, app: Chalice, config: Config) -> None:
         self._app = app
         self._config = config
 
-    def invoke(self, FunctionName: str = None, InvocationType: str = "Event", Payload: str = None):
-        # type: (str, Any) -> InvokeResponse
+    def invoke(
+        self,
+        FunctionName: str = None,
+        InvocationType: str = "Event",
+        Payload: str = None,
+    ) -> InvokeResponse:
         if Payload is None:
-            Payload = '{}'
+            Payload = "{}"
         scoped = self._config.scope(self._config.chalice_stage, FunctionName)
         lambda_context = LambdaContext(
-            FunctionName, memory_size=scoped.lambda_memory_size)
+            FunctionName, memory_size=scoped.lambda_memory_size
+        )
 
         with self._patched_env_vars(scoped.environment_variables):
             response = self._app(json.loads(Payload), lambda_context)
@@ -46,12 +49,11 @@ class ChaliceSlackRequestHandler:
         self.logger = get_bolt_app_logger(app.name, ChaliceSlackRequestHandler)
 
         lambda_client = None
-        if getenv('AWS_CHALICE_CLI_MODE') == 'true':
+        if getenv("AWS_CHALICE_CLI_MODE") == "true":
             lambda_client = LocalLambdaClient(self.chalice, Config())
 
         self.app.listener_runner.lazy_listener_runner = ChaliceLazyListenerRunner(
-            logger=self.logger,
-            lambda_client=lambda_client
+            logger=self.logger, lambda_client=lambda_client
         )
 
         if self.app.oauth_flow is not None:
