51edf84b3fc2efce88782e894c63f080a511d898
Kazuhiro Sera <ksera@slack-corp.com>
2021-05-24 15:24:46 +0900

    Update the Django example (#348)

--- examples/django/manage.py
+++ examples/django/manage.py
@@ -5,7 +5,8 @@ import sys
 
 
 def main():
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "slackapp.settings")
+    """Run administrative tasks."""
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "myslackapp.settings")
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
--- examples/django/slackapp/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path("slack/events", views.events, name="handle"),
-    path("slack/install", views.oauth, name="install"),
-    path("slack/oauth_redirect", views.oauth, name="oauth_redirect"),
-]
