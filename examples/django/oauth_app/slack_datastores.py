7920f3923a39b1d489c95d0ac34bd088e3081996
Kazuhiro Sera <seratch@gmail.com>
2021-07-16 09:48:45 +0900

    Update the Django example for token rotation

--- examples/django/oauth_app/slack_datastores.py
+++ examples/django/oauth_app/slack_datastores.py
@@ -33,13 +33,50 @@ class DjangoInstallationStore(InstallationStore):
         i = installation.to_dict()
         if is_naive(i["installed_at"]):
             i["installed_at"] = make_aware(i["installed_at"])
+        if "bot_token_expires_at" in i and is_naive(i["bot_token_expires_at"]):
+            i["bot_token_expires_at"] = make_aware(i["bot_token_expires_at"])
+        if "user_token_expires_at" in i and is_naive(i["user_token_expires_at"]):
+            i["user_token_expires_at"] = make_aware(i["user_token_expires_at"])
         i["client_id"] = self.client_id
-        SlackInstallation(**i).save()
-        b = installation.to_bot().to_dict()
+        row_to_update = (
+            SlackInstallation.objects.filter(client_id=self.client_id)
+            .filter(enterprise_id=installation.enterprise_id)
+            .filter(team_id=installation.team_id)
+            .filter(installed_at=i["installed_at"])
+            .first()
+        )
+        if row_to_update is not None:
+            for key, value in i.items():
+                setattr(row_to_update, key, value)
+            row_to_update.save()
+        else:
+            SlackInstallation(**i).save()
+
+        self.save_bot(installation.to_bot())
+
+    def save_bot(self, bot: Bot):
+        b = bot.to_dict()
         if is_naive(b["installed_at"]):
             b["installed_at"] = make_aware(b["installed_at"])
+        if "bot_token_expires_at" in b is not None and is_naive(
+            b["bot_token_expires_at"]
+        ):
+            b["bot_token_expires_at"] = make_aware(b["bot_token_expires_at"])
         b["client_id"] = self.client_id
-        SlackBot(**b).save()
+
+        row_to_update = (
+            SlackBot.objects.filter(client_id=self.client_id)
+            .filter(enterprise_id=bot.enterprise_id)
+            .filter(team_id=bot.team_id)
+            .filter(installed_at=b["installed_at"])
+            .first()
+        )
+        if row_to_update is not None:
+            for key, value in b.items():
+                setattr(row_to_update, key, value)
+            row_to_update.save()
+        else:
+            SlackBot(**b).save()
 
     def find_bot(
         self,
@@ -53,7 +90,8 @@ class DjangoInstallationStore(InstallationStore):
         if is_enterprise_install:
             t_id = None
         rows = (
-            SlackBot.objects.filter(enterprise_id=e_id)
+            SlackBot.objects.filter(client_id=self.client_id)
+            .filter(enterprise_id=e_id)
             .filter(team_id=t_id)
             .order_by(F("installed_at").desc())[:1]
         )
@@ -64,10 +102,12 @@ class DjangoInstallationStore(InstallationStore):
                 enterprise_id=b.enterprise_id,
                 team_id=b.team_id,
                 bot_token=b.bot_token,
+                bot_refresh_token=b.bot_refresh_token,
+                bot_token_expires_at=b.bot_token_expires_at,
                 bot_id=b.bot_id,
                 bot_user_id=b.bot_user_id,
                 bot_scopes=b.bot_scopes,
-                installed_at=b.installed_at.timestamp(),
+                installed_at=b.installed_at,
             )
         return None
 
@@ -85,13 +125,15 @@ class DjangoInstallationStore(InstallationStore):
             t_id = None
         if user_id is None:
             rows = (
-                SlackInstallation.objects.filter(enterprise_id=e_id)
+                SlackInstallation.objects.filter(client_id=self.client_id)
+                .filter(enterprise_id=e_id)
                 .filter(team_id=t_id)
                 .order_by(F("installed_at").desc())[:1]
             )
         else:
             rows = (
-                SlackInstallation.objects.filter(enterprise_id=e_id)
+                SlackInstallation.objects.filter(client_id=self.client_id)
+                .filter(enterprise_id=e_id)
                 .filter(team_id=t_id)
                 .filter(user_id=user_id)
                 .order_by(F("installed_at").desc())[:1]
@@ -104,16 +146,20 @@ class DjangoInstallationStore(InstallationStore):
                 enterprise_id=i.enterprise_id,
                 team_id=i.team_id,
                 bot_token=i.bot_token,
+                bot_refresh_token=i.bot_refresh_token,
+                bot_token_expires_at=i.bot_token_expires_at,
                 bot_id=i.bot_id,
                 bot_user_id=i.bot_user_id,
                 bot_scopes=i.bot_scopes,
                 user_id=i.user_id,
                 user_token=i.user_token,
+                user_refresh_token=i.user_refresh_token,
+                user_token_expires_at=i.user_token_expires_at,
                 user_scopes=i.user_scopes,
                 incoming_webhook_url=i.incoming_webhook_url,
                 incoming_webhook_channel_id=i.incoming_webhook_channel_id,
                 incoming_webhook_configuration_url=i.incoming_webhook_configuration_url,
-                installed_at=i.installed_at.timestamp(),
+                installed_at=i.installed_at,
             )
         return None
 
