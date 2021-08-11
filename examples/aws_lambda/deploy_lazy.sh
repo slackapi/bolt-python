6f11e8e6f07bb9328d72cede67dcf99d0fda9f67
Filip Maj <fmaj@slack-corp.com>
2021-08-11 16:32:35 -0400

    Added basic lazy lambda exmaple setup and deploy instructions. Fixed vendor module import issues.

--- examples/aws_lambda/deploy_lazy.sh
+++ examples/aws_lambda/deploy_lazy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-rm -rf vendor && mkdir -p vendor/slack_bolt && cp -pr ../../slack_bolt/* vendor/slack_bolt/
+rm -rf slack_bolt && mkdir slack_bolt && cp -pr ../../slack_bolt/* slack_bolt/
 pip install python-lambda -U
 lambda deploy \
   --config-file lazy_aws_lambda_config.yaml \
