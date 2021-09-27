19fd9b0397a2a278f52bce50ffb9fb0e6cd7cd1c
Kazuhiro Sera <seratch@gmail.com>
2021-09-28 06:17:28 +0900

    Apply suggestions from code review
    Co-authored-by: Fil Maj <maj.fil@gmail.com>
    Co-authored-by: Alissa Renz <alissa.renz@gmail.com>

--- tests/slack_bolt/oauth/test_internals.py
+++ tests/slack_bolt/oauth/test_internals.py
@@ -4,7 +4,7 @@ from slack_bolt.oauth.internals import build_detailed_error
 class TestOAuthInternals:
     def test_build_detailed_error_invalid_browser(self):
         result = build_detailed_error("invalid_browser")
-        assert result.startswith("invalid_browser: Perhaps, you reloaded this page or")
+        assert result.startswith("invalid_browser: This can occur due to page reload, ")
 
     def test_build_detailed_error_invalid_state(self):
         result = build_detailed_error("invalid_state")
@@ -21,7 +21,7 @@ class TestOAuthInternals:
     def test_build_detailed_error_storage_error(self):
         result = build_detailed_error("storage_error")
         assert result.startswith(
-            "storage_error: The app's server-side has some issue. Contact the app developer."
+            "storage_error: The app's server encountered an issue. Contact the app developer."
         )
 
     def test_build_detailed_error_others(self):
