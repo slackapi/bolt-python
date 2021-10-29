292e9c7ec2855612c33fa3192dc721c32ea5ae2d
D Rutter <47083572+dkzk22@users.noreply.github.com>
2021-10-29 15:30:26 +0100

    fix perform_token_rotation kwarg (#503)

--- slack_bolt/authorization/async_authorize.py
+++ slack_bolt/authorization/async_authorize.py
@@ -209,7 +209,7 @@ class AsyncInstallationStoreAuthorize(AsyncAuthorize):
                             raise BoltError(self._config_error_message)
                         refreshed = await self.token_rotator.perform_token_rotation(
                             installation=installation,
-                            token_rotation_expiration_minutes=self.token_rotation_expiration_minutes,
+                            minutes_before_expiration=self.token_rotation_expiration_minutes,
                         )
                         if refreshed is not None:
                             await self.installation_store.async_save(refreshed)
