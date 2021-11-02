4d6448462468fe8abee80b8ad3e94dda980803bb
Kazuhiro Sera <ksera@slack-corp.com>
2021-11-02 18:23:15 +0900

    Upgrade pytype version to 2021.10.25 (#511)

--- scripts/run_pytype.sh
+++ scripts/run_pytype.sh
@@ -5,5 +5,5 @@ script_dir=$(dirname $0)
 cd ${script_dir}/.. && \
   pip install -e ".[async]" && \
   pip install -e ".[adapter]" && \
-  pip install "pytype==2021.10.11" && \
+  pip install "pytype==2021.10.25" && \
   pytype slack_bolt/
