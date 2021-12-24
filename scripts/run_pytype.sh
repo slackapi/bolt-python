0b980ecfd1422dec7596232c9988d129d2d95032
Kazuhiro Sera <seratch@gmail.com>
2021-12-24 11:40:08 +0900

    Upgrade pytype to 2021.12.15

--- scripts/run_pytype.sh
+++ scripts/run_pytype.sh
@@ -5,5 +5,5 @@ script_dir=$(dirname $0)
 cd ${script_dir}/.. && \
   pip install -e ".[async]" && \
   pip install -e ".[adapter]" && \
-  pip install "pytype==2021.12.8" && \
+  pip install "pytype==2021.12.15" && \
   pytype slack_bolt/
