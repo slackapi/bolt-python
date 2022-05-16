39fcb60215021111b7e08d1a805ede7df689bc18
Kazuhiro Sera <ksera@slack-corp.com>
2022-05-16 16:16:01 +0900

    Add Google Cloud Functions adapter (ref #646)

--- examples/google_cloud_functions/simple_main.py
+++ examples/google_cloud_functions/simple_main.py
@@ -22,23 +22,41 @@ def event_test(body, say, logger):
 
 
 # Flask adapter
-from slack_bolt.adapter.flask import SlackRequestHandler
+# TODO: Once this once a new version newer than 1.13.2, delete handler and enable the slack_bolt.adapter import instead
+# from slack_bolt.adapter.google_cloud_functions import SlackRequestHandler
+from handler import SlackRequestHandler
+from flask import Request
+
 
 handler = SlackRequestHandler(app)
 
 
 # Cloud Function
-def hello_bolt_app(request):
+def hello_bolt_app(req: Request):
     """HTTP Cloud Function.
     Args:
-        request (flask.Request): The request object.
+        req (flask.Request): The request object.
         <https://flask.palletsprojects.com/en/1.1.x/api/#incoming-request-data>
     Returns:
         The response text, or any set of values that can be turned into a
         Response object using `make_response`
         <https://flask.palletsprojects.com/en/1.1.x/api/#flask.make_response>.
     """
-    return handler.handle(request)
+    return handler.handle(req)
+
+
+# For local development
+# python main.py
+if __name__ == "__main__":
+    from flask import Flask, request
+
+    flask_app = Flask(__name__)
+
+    @flask_app.route("/function", methods=["GET", "POST"])
+    def handle_anything():
+        return handler.handle(request)
+
+    flask_app.run(port=3000)
 
 
 # Step1: Create a new Slack App: https://api.slack.com/apps
@@ -53,7 +71,8 @@ def hello_bolt_app(request):
 # gcloud config set project YOUR_PROJECT_NAME
 
 # Step4: Deploy a function in the project
-# gcloud functions deploy hello_bolt_app --runtime python38 --trigger-http --allow-unauthenticated --env-vars-file .env.yaml
+# cp simple_main.py main.py
+# gcloud functions deploy hello_bolt_app --runtime python39 --trigger-http --allow-unauthenticated --env-vars-file .env.yaml
 # gcloud functions describe hello_bolt_app
 
 # Step5: Set Request URL
