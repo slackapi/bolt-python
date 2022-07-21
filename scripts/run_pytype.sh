dbdb2c58d07c76ec1d4359c7f12128c680858303
Kazuhiro Sera <ksera@slack-corp.com>
2022-07-21 15:07:25 +0900

    Upgrade pytype to the latest version

--- scripts/run_pytype.sh
+++ scripts/run_pytype.sh
@@ -5,5 +5,5 @@ script_dir=$(dirname $0)
 cd ${script_dir}/.. && \
   pip install -e ".[async]" && \
   pip install -e ".[adapter]" && \
-  pip install "pytype==2022.6.30" && \
+  pip install "pytype==2022.7.18" && \
   pytype slack_bolt/
