f64928ff0dd63f3e2a9a08591def8d9e50a7d17e
William Bergamin <wbergamin@slack-corp.com>
2022-08-03 17:03:32 -0400

    Cleaned up code

--- slack_bolt/cli/__init__.py
+++ slack_bolt/cli/__init__.py
@@ -1,15 +1,2 @@
 """Cli modules to allow Bolt apps to interact with the bolt cli.
 """
-
-
-def handle_exception(func):
-    def wrapper(*args, **kwargs):
-        try:
-            return func(*args, **kwargs)
-        except KeyboardInterrupt:
-            print("Shutdown requested... goodbye!")
-        except Exception as e:
-            print(e)
-            exit()
-
-    return wrapper
