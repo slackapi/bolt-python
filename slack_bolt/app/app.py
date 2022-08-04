30937d46c8ffaad9b90addbdcfdf50f8d4907e94
William Bergamin <wbergamin@slack-corp.com>
2022-08-04 19:19:09 -0400

    end of day commit

--- slack_bolt/app/app.py
+++ slack_bolt/app/app.py
@@ -803,14 +803,14 @@ class App:
 
             # Use this method as a decorator
             @app.function("reverse")
-            def reverse_string(event, success: Success, err: Error):
+            def reverse_string(event, complete_success: CompleteSuccess, complete_error: CompleteError):
                 try:
                     string_to_reverse = event["inputs"]["stringToReverse"]
-                    success({
+                    complete_success({
                         "reverseString": string_to_reverse[::-1]
                     })
                 except Exception as e:
-                    err("Cannot reverse string")
+                    complete_error("Cannot reverse string")
                     raise e
 
             # Pass a function to this method
