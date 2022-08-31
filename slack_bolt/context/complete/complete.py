1c5b314ce90a5e41ea81e6508fd145ba6230c736
William Bergamin <wbergamin@slack-corp.com>
2022-08-31 18:05:44 -0400

    improved complete() behaviour

--- slack_bolt/context/complete/complete.py
+++ slack_bolt/context/complete/complete.py
@@ -2,7 +2,7 @@ from typing import Optional, Union
 
 from slack_sdk import WebClient
 from slack_sdk.web import SlackResponse
-from slack_bolt.context.complete.internals import can_not_complete
+from slack_bolt.context.complete.internals import can_not_complete, get_kwargs_msg, get_name_msg
 
 
 class Complete:
@@ -20,25 +20,18 @@ class Complete:
     def __call__(self, *args, outputs: Optional[Union[str, dict]] = None, error: Optional[str] = None) -> SlackResponse:
         if args:
             raise TypeError(
-                f"{self._name()} takes 0 positional arguments but "
+                f"{get_name_msg(self)} takes 0 positional arguments but "
                 f"{len(args)} {'were' if len(args) > 1 else 'was'} given, "
-                f"missing 1 keyword-only argument: {self._get_kwargs_msg()}"
+                f"missing 1 keyword-only argument: {get_kwargs_msg(self)}"
             )
 
         if outputs is not None and error is not None:
             raise TypeError(
-                f"{self._name()} expects 1 keyword-only argument but 2 were given, " f"provide: {self._get_kwargs_msg()}"
+                f"{get_name_msg(self)} expects 1 keyword-only argument but 2 were given, provide: {get_kwargs_msg(self)}"
             )
 
         if can_not_complete(self):
-            raise ValueError(f"{self._name()} is unsupported here as there is no function_execution_id")
-
-        if isinstance(outputs, dict) or isinstance(outputs, str):
-            # TODO add this new api call to the sdk and use it here
-            return self.client.api_call(
-                "functions.completeSuccess",
-                json={"outputs": outputs, "function_execution_id": self.function_execution_id},
-            )
+            raise ValueError(f"{get_name_msg(self)} is unsupported here as there is no function_execution_id")
 
         if isinstance(error, str):
             # TODO add this new api call to the sdk and use it here
@@ -49,12 +42,5 @@ class Complete:
         # TODO add this new api call to the sdk and use it here
         return self.client.api_call(
             "functions.completeSuccess",
-            json={"outputs": {}, "function_execution_id": self.function_execution_id},
+            json={"outputs": outputs, "function_execution_id": self.function_execution_id},
         )
-
-    def _get_kwargs_msg(self) -> str:
-        separator = "', '"
-        return f"'{separator.join(self.__call__.__kwdefaults__.keys())}' or no arguments"
-
-    def _name(self) -> str:
-        return f"{Complete.__name__.lower()}()"
--- slack_bolt/context/complete/internals.py
+++ slack_bolt/context/complete/internals.py
@@ -3,3 +3,12 @@ from typing import Any
 
 def can_not_complete(self: Any) -> bool:
     return not hasattr(self, "client") or self.client is None or self.function_execution_id is None
+
+
+def get_kwargs_msg(self: Any) -> str:
+    separator = "', '"
+    return f"'{separator.join(self.__call__.__kwdefaults__.keys())}' or no arguments"
+
+
+def get_name_msg(self: Any) -> str:
+    return f"{self.__class__.__name__.lower()}()"
