8d4ef1d29130199b5c2b1b25a53a10ae2210d47b
Kazuhiro Sera <seratch@gmail.com>
2022-10-26 17:39:57 +0900

    Add Python 3.11 to the supported language versions

--- setup.py
+++ setup.py
@@ -109,6 +109,7 @@ setuptools.setup(
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
         "Programming Language :: Python :: Implementation :: CPython",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
