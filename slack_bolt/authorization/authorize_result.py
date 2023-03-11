6c46fda356a62173ba72023a0fff2381c98fc8c2
Kazuhiro Sera <seratch@gmail.com>
2023-03-11 15:06:40 +0900

    Enhance AuthorizeResult to have bot/user_scopes when they're available (optional) Also, this commit fixes the bug where authorize_result.user_id is not resolved when both bot and user tokens exist.

--- slack_bolt/authorization/authorize_result.py
+++ slack_bolt/authorization/authorize_result.py
@@ -1,4 +1,4 @@
-from typing import Optional
+from typing import Optional, List, Union
 
 from slack_sdk.web import SlackResponse
 
@@ -11,8 +11,10 @@ class AuthorizeResult(dict):
     bot_id: Optional[str]
     bot_user_id: Optional[str]
     bot_token: Optional[str]
+    bot_scopes: Optional[List[str]]  # since v1.17
     user_id: Optional[str]
     user_token: Optional[str]
+    user_scopes: Optional[List[str]]  # since v1.17
 
     def __init__(
         self,
@@ -23,9 +25,11 @@ class AuthorizeResult(dict):
         bot_user_id: Optional[str] = None,
         bot_id: Optional[str] = None,
         bot_token: Optional[str] = None,
+        bot_scopes: Optional[Union[List[str], str]] = None,
         # user
         user_id: Optional[str] = None,
         user_token: Optional[str] = None,
+        user_scopes: Optional[Union[List[str], str]] = None,
     ):
         """
         Args:
@@ -34,8 +38,10 @@ class AuthorizeResult(dict):
             bot_user_id: Bot user's User ID starting with either `U` or `W`
             bot_id: Bot ID starting with `B`
             bot_token: Bot user access token starting with `xoxb-`
+            bot_scopes: The scopes associated with the bot token
             user_id: The request user ID
             user_token: User access token starting with `xoxp-`
+            user_scopes: The scopes associated wth the user token
         """
         self["enterprise_id"] = self.enterprise_id = enterprise_id
         self["team_id"] = self.team_id = team_id
@@ -43,9 +49,15 @@ class AuthorizeResult(dict):
         self["bot_user_id"] = self.bot_user_id = bot_user_id
         self["bot_id"] = self.bot_id = bot_id
         self["bot_token"] = self.bot_token = bot_token
+        if bot_scopes is not None and isinstance(bot_scopes, str):
+            bot_scopes = [scope.strip() for scope in bot_scopes.split(",")]
+        self["bot_scopes"] = self.bot_scopes = bot_scopes
         # user
         self["user_id"] = self.user_id = user_id
         self["user_token"] = self.user_token = user_token
+        if user_scopes is not None and isinstance(user_scopes, str):
+            user_scopes = [scope.strip() for scope in user_scopes.split(",")]
+        self["user_scopes"] = self.user_scopes = user_scopes
 
     @classmethod
     def from_auth_test_response(
@@ -53,7 +65,10 @@ class AuthorizeResult(dict):
         *,
         bot_token: Optional[str] = None,
         user_token: Optional[str] = None,
+        bot_scopes: Optional[Union[List[str], str]] = None,
+        user_scopes: Optional[Union[List[str], str]] = None,
         auth_test_response: SlackResponse,
+        user_auth_test_response: Optional[SlackResponse] = None,
     ) -> "AuthorizeResult":
         bot_user_id: Optional[str] = (  # type:ignore
             auth_test_response.get("user_id") if auth_test_response.get("bot_id") is not None else None
@@ -61,12 +76,18 @@ class AuthorizeResult(dict):
         user_id: Optional[str] = (  # type:ignore
             auth_test_response.get("user_id") if auth_test_response.get("bot_id") is None else None
         )
+        # Since v1.28, user_id can be set when user_token w/ its auth.test response exists
+        if user_id is None and user_auth_test_response is not None:
+            user_id: Optional[str] = user_auth_test_response.get("user_id")  # type:ignore
+
         return AuthorizeResult(
             enterprise_id=auth_test_response.get("enterprise_id"),
             team_id=auth_test_response.get("team_id"),
             bot_id=auth_test_response.get("bot_id"),
             bot_user_id=bot_user_id,
+            bot_scopes=bot_scopes,
             user_id=user_id,
             bot_token=bot_token,
             user_token=user_token,
+            user_scopes=user_scopes,
         )
