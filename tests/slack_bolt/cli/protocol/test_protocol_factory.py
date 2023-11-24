761e4d7a77e8ce9b2099d698d0858260bd5b3e83
William Bergamin <wbergamin@slack-corp.com>
2023-11-24 11:41:32 -0500

    Update test_protocol_factory.py

--- tests/slack_bolt/cli/protocol/test_protocol_factory.py
+++ tests/slack_bolt/cli/protocol/test_protocol_factory.py
@@ -1,13 +1,15 @@
-from slack_bolt.cli.protocol import protocol_factory, DefaultProtocol, MessageBoundaryProtocol
+from slack_bolt.cli.protocol import protocol_factory, DefaultProtocol, MessageBoundaryProtocol, Protocol
 
 
 class TestProtocolFactory:
     def test_default(self):
         args = []
         protocol = protocol_factory(args)
+        assert isinstance(protocol, Protocol)
         assert isinstance(protocol, DefaultProtocol)
 
     def test_message_boundaries(self):
         args = [f"--protocol={MessageBoundaryProtocol.name}", "--bound=boundary"]
         protocol = protocol_factory(args)
+        assert isinstance(protocol, Protocol)
         assert isinstance(protocol, MessageBoundaryProtocol)
