c87959d390abcac2230a31b428a1df3ce0798741
William Bergamin <wbergamin@slack-corp.com>
2023-11-28 12:00:05 -0500

    fix pytype script

--- scripts/run_pytype.sh
+++ scripts/run_pytype.sh
@@ -3,6 +3,7 @@
 
 script_dir=$(dirname $0)
 cd ${script_dir}/.. && \
+  pip install .
   pip install -r requirements/async.txt && \
   pip install -r requirements/adapter.txt && \
   pip install "pytype==2022.12.15" && \
