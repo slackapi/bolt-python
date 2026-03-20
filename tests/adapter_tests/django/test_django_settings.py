8d8c3d23fb92dc1291d45311da34f891125bba0c
William Bergamin <wbergamin@slack-corp.com>
2026-03-20 11:27:09 -0400

    chore: fix django warnings in tests

--- tests/adapter_tests/django/test_django_settings.py
+++ tests/adapter_tests/django/test_django_settings.py
@@ -2,9 +2,10 @@ SECRET_KEY = "XXX"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
-        "NAME": "logs/db.sqlite3",
+        "NAME": ":memory:",
     }
 }
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 # Django 4 warning: The default value of USE_TZ will change from False to True in Django 5.0.
 # Set USE_TZ to False in your project settings if you want to keep the current default behavior.
 USE_TZ = False
