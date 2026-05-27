b07403e8ab35edcc754032bb30404e285ed9ee86
William Bergamin <wbergamin@slack-corp.com>
2026-05-27 14:42:07 -0400

    chore: rename requirements files to improve Dependabot dependency scoping

--- scripts/format.sh
+++ scripts/format.sh
@@ -7,7 +7,7 @@ cd ${script_dir}/..
 if [[ "$1" != "--no-install" ]]; then
     export PIP_REQUIRE_VIRTUALENV=1
     pip install -U pip
-    pip install -U -r requirements/tools.txt
+    pip install -U -r requirements/dev_tools.txt
 fi
 
 black slack_bolt/ tests/
--- scripts/lint.sh
+++ scripts/lint.sh
@@ -6,7 +6,7 @@ cd ${script_dir}/..
 
 if [[ "$1" != "--no-install" ]]; then
     pip install -U pip
-    pip install -U -r requirements/tools.txt
+    pip install -U -r requirements/dev_tools.txt
 fi
 
 flake8 slack_bolt/ && flake8 examples/
